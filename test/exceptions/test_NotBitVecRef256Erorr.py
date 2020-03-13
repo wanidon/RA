e830c1d3b732ac3021652dcb9152c1ca42dc17f5
wanidon <wwwanidon@gmail.com>
2020-03-13 17:06:45 +0900

    master thesis ver.

--- test/exceptions/test_NotBitVecRef256Erorr.py
+++
@@ -1,9 +0,0 @@
-#import sys, os
-from z3 import *
-from exceptions import NotBitVecRef256Erorr
-
-gpdir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(gpdir)
-
-raise NotBitVecRef256Erorr("hoge.")
-raise NotBitVecRef256Erorr()
--- test/test_template.py
+++
@@ -1,7 +0,0 @@
-import sys, os
-from z3 import simplify
-from utils import *
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-
-
